ee3d1a84b5564714e7bd8629bc14b7d52a4da571
Neotter <tinkleidea@hotmail.com>
2020-02-07 23:32:29 +0800

    4. Acc

--- SourceCode/4.median-of-two-sorted-arrays.py
+++ SourceCode/4.median-of-two-sorted-arrays.py
@@ -45,7 +45,47 @@
 
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        medianOfAll = (len(nums1) + len(nums2)) / 2
+        maxValue = float("inf")
+        minValue = float("-inf")
+        # get length of nums 1&2
+        lenN1 = len(nums1)
+        lenN2 = len(nums2)
+        # choise the shortest array as the binary search array
+        if lenN1 > lenN2:
+            return self.findMedianSortedArrays(nums2, nums1)
+        # if nums1 is empty, nums2 contain the median, return it directly
+        if lenN1 == 0:
+            if lenN2 % 2 == 0:
+                return ((nums2[int(lenN2 / 2) - 1] + nums2[int((lenN2) / 2)]) / 2)
+            else:
+                return nums2[int(lenN2/2)]
+        lenAll = lenN1 + lenN2
+        # cutting range
+        cutL = 0
+        cutR = lenN1
+        # cutting position
+        cutN1 = int(lenN1 / 2)
+        cutN2 = lenAll / 2 - cutN1
 
+        while cutN1 <= lenN1:
+            cutN1 = int((cutR - cutL) / 2 + cutL)
+            cutN2 = int(lenAll / 2 - cutN1)
 
+            L1 = minValue if cutN1 == 0 else nums1[cutN1 - 1]
+            R1 = maxValue if cutN1 == lenN1 else nums1[cutN1]
+            L2 = minValue if cutN2 == 0 else nums2[cutN2 - 1]
+            R2 = maxValue if cutN2 == lenN2 else nums2[cutN2]
+            if L1 > R2:
+                cutR = cutN1 - 1
+            elif L2 > R1:
+                cutL = cutN1 + 1
+            else:
+                if lenAll % 2 == 0:
+                    L1 = max(L1, L2)
+                    R1 = min(R1, R2)
+                    return (L1 + R1) / 2
+                else:
+                    R1 = min(R1, R2)
+                    return R1
+        return -1
 # @lc code=end

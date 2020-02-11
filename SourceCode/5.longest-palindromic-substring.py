52bde4bf9d4470ce8c882249aab057e601307af6
Neotter <tinkleidea@hotmail.com>
2020-02-12 00:30:44 +0800

    5.ACC

--- SourceCode/5.longest-palindromic-substring.py
+++ SourceCode/5.longest-palindromic-substring.py
@@ -3,6 +3,35 @@
 #
 # [5] Longest Palindromic Substring
 #
+# https://leetcode.com/problems/longest-palindromic-substring/description/
+#
+# algorithms
+# Medium (28.54%)
+# Likes:    5150
+# Dislikes: 449
+# Total Accepted:    761.2K
+# Total Submissions: 2.7M
+# Testcase Example:  '"babad"'
+#
+# Given a string s, find the longest palindromic substring in s. You may assume
+# that the maximum length of s is 1000.
+#
+# Example 1:
+#
+#
+# Input: "babad"
+# Output: "bab"
+# Note: "aba" is also a valid answer.
+#
+#
+# Example 2:
+#
+#
+# Input: "cbbd"
+# Output: "bb"
+#
+#
+#
 
 # @lc code=start
 
@@ -10,20 +39,17 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
         lenS = len(s)
+        # only one element, return directly
         if lenS <= 1:
             return s
-        dp = [[None for i in range(lenS)] for j in range(lenS)]
-        resLeft = resRight = 0
         # init dp
-        for i in range(lenS):
-            dp[i][i] = True
-            dp[i][i-1] = True
-        for lenP in range(2, lenS):
-            for i in range(lenS - lenP):
-                if s[i] == s[i + lenP - 1] and dp[i + 1][i + lenP - 2]:
-                    dp[i][i + lenP - 1] = True
-                    if resLeft - resLeft + 1 < lenP:
-                        resLeft = i
-                        resRight = i + lenP - 1
-        return s[resLeft:resRight-resLeft+1]
+        dp = [[None for i in range(lenS)] for j in range(lenS)]
+        res = ""
+        for i in range(lenS - 1, -1, -1):
+            for j in range(i, lenS):
+                dp[i][j] = (s[i] == s[j]) and (j - i < 3 or dp[i + 1][j - 1])
+                if dp[i][j] and (res == "" or j - i + 1 > len(res)):
+                    res = s[i:j + 1]
+        return res
+
 # @lc code=end

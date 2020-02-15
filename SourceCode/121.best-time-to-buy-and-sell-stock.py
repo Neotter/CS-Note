316b32c6e1c46e5d5a3462202a685d475fca65fc
Neotter <tinkleidea@hotmail.com>
2020-02-15 13:45:32 +0800

    121. ACC

--- SourceCode/121.best-time-to-buy-and-sell-stock.py
+++ SourceCode/121.best-time-to-buy-and-sell-stock.py
@@ -47,5 +47,10 @@
 
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        pass
+        max_profit, min_price = 0, float('inf')
+        for price in prices:
+            min_price = min(min_price, price)
+            max_profit = max(max_profit, price - min_price)
+        return max_profit
+
 # @lc code=end
